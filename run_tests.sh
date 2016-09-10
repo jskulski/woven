22445dfa2fa4fb21d7b0e9004833c9b14b4f0122
Jon Skulski <jskulski@gmail.com>
2016-09-10 14:20:34 -0700

    Server in dockerfile, correctly setup and torndown

--- run_tests.sh
+++ run_tests.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
+./test.sh
 fswatch -o test.sh test-app server | xargs -n1 ./test.sh
--- test.sh
+++ test.sh
@@ -8,7 +8,8 @@ TEST_BUILD_DIR='TEST-BUILD-DIR'
 TEST_APP_DIR='test-app'
 
 SERVER_SRC_DIR='server'
-SERVER_DOCKER_IMAGE_TAG_NAME='acceptance-test-woven-server'
+SERVER_DOCKER_IMAGE_NAME='acceptance-test-woven-server-image'
+SERVER_DOCKER_CONTAINER_NAME='acceptance-test-woven-server-container'
 
 # Setup
 mkdir -p $TEST_BUILD_DIR
@@ -21,8 +22,15 @@ cp -r $TEST_APP_DIR $TEST_BUILD_DIR
 (cd $TEST_BUILD_DIR && git add . && git commit -m "Adding app")
 
 # Setup Woven server
-docker build --no-cache $SERVER_SRC_DIR -t $SERVER_DOCKER_IMAGE_TAG_NAME
-docker rmi $SERVER_DOCKER_IMAGE_TAG_NAME
+docker build --no-cache $SERVER_SRC_DIR -t $SERVER_DOCKER_IMAGE_NAME
+docker run -d -p 8080:80 --name $SERVER_DOCKER_CONTAINER_NAME $SERVER_DOCKER_IMAGE_NAME
+
+echo "check out ... http://localhost:8080"
+sleep 50
+
+docker stop $SERVER_DOCKER_CONTAINER_NAME
+docker rm $SERVER_DOCKER_CONTAINER_NAME
+docker rmi $SERVER_DOCKER_IMAGE_NAME
 
 # Cleanup
 rm -rf $TEST_BUILD_DIR
